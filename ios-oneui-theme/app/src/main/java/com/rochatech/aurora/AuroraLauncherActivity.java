79cba4a55f6bae88c19c9368faead332cdc284a2
rochatech-spec <flavioroch.nt@gmail.com>
2026-09-06 17:16:02 -0300

    Remove Aurora launcher project

--- ios-oneui-theme/app/src/main/java/com/rochatech/aurora/AuroraLauncherActivity.java
+++
@@ -1,5 +0,0 @@
-package com.rochatech.aurora;
-
-/** HOME entry point for the stable Premium Light launcher engine. */
-public class AuroraLauncherActivity extends StablePremiumLauncherActivity {
-}
