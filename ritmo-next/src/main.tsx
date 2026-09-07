b7e958837e21fab4d1bc6282c465b4ead93221c7
rochatech-spec <flavioroch.nt@gmail.com>
2026-09-06 22:50:09 -0300

    feat(ritmo): load iOS parity styles

--- ritmo-next/src/main.tsx
+++ ritmo-next/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import App from './App'
 import './styles.css'
 import './security.css'
+import './ios-parity.css'
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
