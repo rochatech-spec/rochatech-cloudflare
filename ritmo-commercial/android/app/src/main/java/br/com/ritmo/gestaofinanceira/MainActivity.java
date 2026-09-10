a86862f367da61e8ec3882a5367683279465cf60
rochatech-spec <flavioroch.nt@gmail.com>
2026-09-10 20:39:42 -0300

    refactor(ritmo): remover codigo Android e Tauri

--- ritmo-commercial/android/app/src/main/java/br/com/ritmo/gestaofinanceira/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package br.com.ritmo.gestaofinanceira;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
--- ritmo-commercial/src-tauri/src/main.rs
+++
@@ -1,5 +0,0 @@
-#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-
-fn main() {
-    ritmo_lib::run();
-}
